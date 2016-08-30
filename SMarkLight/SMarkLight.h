97e1becadd413ddb3812492961b2595ca125b3b0
CodeEagle <stasura@hotmail.com>
2016-08-30 17:26:42 +0800

    Initial Commit

---
+++ SMarkLight/SMarkLight.h
@@ -0,0 +1,19 @@
+//
+//  SMarkLight.h
+//  SMarkLight
+//
+//  Created by LawLincoln on 16/8/30.
+//  Copyright © 2016年 LawLincoln. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMarkLight.
+FOUNDATION_EXPORT double SMarkLightVersionNumber;
+
+//! Project version string for SMarkLight.
+FOUNDATION_EXPORT const unsigned char SMarkLightVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMarkLight/PublicHeader.h>
+
+
